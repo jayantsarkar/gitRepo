c3cdfedfe4e0834e46d0de8039762129b552900b
jayant <jayant.sarkar@outlook.com>
2013-12-12 15:28:42 +0530

    1st commit from eclipse

---
+++ git1/src/git1/com/jayant/gitHub/HelloWorld.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package git1.com.jayant.gitHub;
+
+/**
+ * @author jasarkar
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+	}
+
+}
