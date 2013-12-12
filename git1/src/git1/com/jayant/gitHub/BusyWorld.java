38ba84369b9a8792e6909bea55eb92b2a84100e7
jayant <jayant.sarkar@outlook.com>
2013-12-12 15:49:24 +0530

    4th commit

--- git1/src/git1/com/jayant/gitHub/BusyWorld.java
+++ git1/src/git1/com/jayant/gitHub/BusyWorld.java
@@ -7,17 +7,14 @@ package git1.com.jayant.gitHub;
  * @author jasarkar
  *
  */
-public class HelloWorld {
+public class BusyWorld {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World");
-		System.out.println("Hello World 2");
-		System.out.println("Hello World 3");
-		System.out.println("Hello World 4");
+		System.out.println("World is busy!!!");
 	}
 
 }
